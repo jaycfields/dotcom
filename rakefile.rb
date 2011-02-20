c597a13cf90acb7127fe3697e1bfa37759f62d4d
Jay Fields <git@jayfields.com>
2011-02-20 12:46:54 -0500

    saving

---
+++ rakefile.rb
@@ -0,0 +1,57 @@
+require 'fileutils'
+require 'erb'
+
+task :publish => :build do
+  require 'net/ftp'
+
+  Net::FTP.open('ftp.jayfields.com') do |ftp|
+    ftp.login('jaycf', 'jaycf1')
+    Dir['build/*'].each do |chapter|
+      ftp.passive = true
+      if chapter =~ /jpg$/
+        puts "attempting to publish binary #{chapter}"
+        ftp.putbinaryfile chapter
+      else
+        puts "attempting to publish text #{chapter}"
+        ftp.puttextfile chapter
+      end
+      puts "published #{chapter}"
+    end
+    ftp.close
+  end
+end
+
+class Content
+  class << self
+    attr_accessor :source, :page
+  end
+  def self.content
+    File.read(source)
+  end
+  def self.page
+    case @page 
+      when "Index" then "Home"
+      when "Oss" then "Open Source"
+      else @page
+    end
+  end
+end
+  
+task :build do
+  FileUtils.rm_rf 'build' if File.exists?('build')
+  FileUtils.mkdir 'build'
+  Dir['./*.txt'].each do |txt|
+    File.open("build/#{File.basename(txt, ".txt")}.htm", "w") do |file|
+      erb_template = ERB.new File.read("naturalist/index.html")
+      Content.source = txt
+      Content.page = File.basename(txt, ".txt").capitalize
+      file << erb_template.result(Content.send(:binding))
+    end
+  end
+  Dir['naturalist/css/*'].each do |file|
+    FileUtils.cp file, "build"
+  end
+  Dir['naturalist/img/*'].each do |file|
+    FileUtils.cp file, "build"
+  end
+end
